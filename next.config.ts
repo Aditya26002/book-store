b50a509f9b8c941699bec72f019907e6cadfa97c
Aditya <aditya2012058@akgec.ac.in>
2025-03-23 14:12:14 +0530

    Add image domains to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "images.unsplash.com",
+      "cdn.pixabay.com",
+      "images.pexels.com",
+    ],
+  },
 };
 
 export default nextConfig;
